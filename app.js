a7784749ed4484bb4c4d3df0aaf58068b79ed598
mnubhan <nubhant@gmail.com>
2023-05-04 21:12:27 +0100

    chore:get all classes and id which will be use in js

---
+++ app.js
@@ -0,0 +1,10 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoredBoard_div = document.getElementsByClassName("score-board");
+const result_div =document.getElementsByClassName("result");
+const rock_div =document.getElementById("r");
+const paper_div =document.getElementById("p");
+const scissor_div =document.getElementById("s");
+
